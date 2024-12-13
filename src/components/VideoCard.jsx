dc608c22a76db835d4682c5ad211bbf0d3a984ce
duclee193 <ducorgan2003@gmail.com>
2024-12-13 20:17:13 +0700

    Q1

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types'; // Fix: Import PropTypes
 import FooterLeft from './FooterLeft';
 import FooterRight from './FooterRight';
 
@@ -16,20 +17,22 @@ const VideoCard = (props) => {
     setVideoRef,
     autoplay
   } = props;
-  
+
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (autoplay) {
+    if (autoplay && videoRef.current) {
       videoRef.current.play();
     }
   }, [autoplay]);
 
   const onVideoPress = () => {
-    if (videoRef.current.paused) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
     }
   };
 
@@ -50,10 +53,10 @@ const VideoCard = (props) => {
         playsInline
         src={url}
       ></video>
-  
+
       <div className="bottom-controls">
         <div className="footer-left">
-          <FooterLeft username={username} description={description} song={song}/>
+          <FooterLeft username={username} description={description} song={song} />
         </div>
         <div className="footer-right">
           <FooterRight
@@ -68,4 +71,32 @@ const VideoCard = (props) => {
     </div>
   );
 };
+
+// PropTypes validation
+VideoCard.propTypes = {
+  url: PropTypes.string.isRequired,
+  username: PropTypes.string,
+  description: PropTypes.string,
+  song: PropTypes.string,
+  likes: PropTypes.number,
+  shares: PropTypes.number,
+  comments: PropTypes.number,
+  saves: PropTypes.number,
+  profilePic: PropTypes.string,
+  setVideoRef: PropTypes.func,
+  autoplay: PropTypes.bool
+};
+
+VideoCard.defaultProps = {
+  username: '',
+  description: '',
+  song: '',
+  likes: 0,
+  shares: 0,
+  comments: 0,
+  saves: 0,
+  profilePic: '',
+  autoplay: false
+};
+
 export default VideoCard;
